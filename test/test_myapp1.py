e7ebce539c924519f819fff781decd6181491b5f
devstream <devstream@merico.dev>
2023-05-26 16:10:06 +0000

    init by devstream

---
+++ test/test_myapp1.py
@@ -0,0 +1,12 @@
+import unittest
+
+from app.myapp1 import hello
+
+
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+
+if __name__ == '__main__':
+    unittest.main()
